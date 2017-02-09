c58b6017207cca7411dec89406ec5aaa58774476
Tim Otten <totten@civicrm.org>
2017-02-09 13:07:51 -0800

    Implement clean(), cleanSync(). Avoid unnecessary cv() call for path lookup.

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 module.exports = function(options) {
   var cvSync = require('civicrm-cv')({mode: 'sync'});
   var cvPromise = require('civicrm-cv')({mode: 'promise'});
-  var buildPath = cvSync("ev 'return CRM_Core_Config::singleton()->templateCompileDir;'");
   var updateScript = __dirname + "/bin/civicrm-scssroot-update.php";
   var cleanScript = __dirname + "/bin/civicrm-scssroot-clean.php";
+  var buildPath = null;
+
+  /**
+   * Assimilate any interesting metadata returned by external script.
+   */
+  function assimilate(result) {
+    if (result.buildPath) buildPath = result.buildPath;
+  }
 
   return {
-    clean: function() {
-    },
 
-    update: function(callback) {
-      cvPromise('scr ' + updateScript).then(function(){
-        callback();
-      });
-    },
-
-    updateSync: function() {
-      cvSync('scr ' + updateScript);
-    },
+    clean: function() { return cvPromise('scr ' + cleanScript).then(assimilate); },
+    cleanSync: function() { assimilate(cvSync('scr ' + cleanScript)); },
+    update: function() { return cvPromise('scr ' + updateScript).then(assimilate); },
+    updateSync: function() { assimilate(cvSync('scr ' + updateScript)); },
 
     getPath: function() {
+      if (buildPath === null) {
+        buildPath = cvSync("ev 'return CRM_Core_Config::singleton()->templateCompileDir;'");
+      }
       return buildPath;
     },
 
@@ -42,7 +45,7 @@ module.exports = function(options) {
         var extKeys = filter;
         filter = function(ext) {
           return extKeys.indexOf(ext.key) >= 0;
-        }
+        };
       }
 
       for (var i = 0; i < extDirs.length; i++) {
